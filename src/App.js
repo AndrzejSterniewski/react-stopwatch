bcc3a716e818b853fd588bb158b19a522e7b6314
Andrzej Sterniewski <drapiszon@gmail.com>
2023-10-19 14:29:03 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./components/Button/Button";
+import Time from "./components/Time/Time";
+import { useEffect, useState } from "react";
 
 function App() {
+
+  const [ time, setTime] = useState(0);
+
+
+  useEffect(() => {
+
+    return () => {
+      if(timer) clearInterval(timer);
+    };
+  }, []);
+
+  const timer = setInterval(() => {...}, 1000);
+
+  setInterval(() =>{
+    setTime(prevValue => prevValue + 1);
+  }, 1)
+
+  const startTime = () => {
+
+  }
+
+  const stopTime = () => {
+    clearInterval(timer);
+  }
+  
+  const resetTime = () => {
+    setTime(0);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Stopwatch</h1>
+      <Time {...time} />
+      <Button onClick={startTime}/>Start<Button/>
+      <Button onCLick={stopTime}>Stop</Button>
+      <Button onClick={resetTime}>Reset</Button>
     </div>
   );
 }
---
+++ src/components/Time/Time.js
@@ -0,0 +1,16 @@
+// import styles from './Time.module.scss';
+
+// const Time = ({ time }) => {
+//    const formatTime = millseconds => {
+//      return ??
+//    };
+
+//   return (
+//     <div className={styles.component}>
+//        {formatTime(time)}
+//     </div>
+//   );
+
+// };
+
+// export default Time;
