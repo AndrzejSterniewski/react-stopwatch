b12e7264f07a8a09613c2ac244e126f766dbaf71
Andrzej Sterniewski <drapiszon@gmail.com>
2023-10-22 21:31:03 +0200

    work on format time function

--- src/components/Time/Time.js
+++ src/components/Time/Time.js
@@ -3,9 +3,13 @@ import styles from './Time.module.scss';
 const Time = ({ time }) => {
   const formatTime = millseconds => {
 
-    let seconds = Math.floor(millseconds / 1000);
-    let minutes = Math.floor(seconds / 60);
-    let hours = Math.floor(minutes / 60);
+    // let seconds = Math.floor(millseconds / 1000);
+    // let minutes = Math.floor(seconds / 60);
+    // let hours = Math.floor(minutes / 60);
+
+    let seconds = Math.floor((millseconds / 1000) % 60);
+    let minutes = Math.floor((seconds / 60) % 60);
+    let hours = Math.floor((minutes / 60) % 24);
 
     return hours + ':' + minutes + ':' + seconds;
   };
